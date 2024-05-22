f9ee006e5d235a100e507c0ebb7dc4ad53d60167
prakash choure <prakashchoure2002@gmail.com>
2024-05-22 11:58:48 +0530

    api done in cart

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -10,7 +10,7 @@ const Cart = () => {
                 // body:JSON.stringify(),
                 // header:{'content-type':'application/json'}
             })
-            console.log("cart",response)
+            console.log("cart details",response)
             setData(response?.data)
             
             
@@ -27,10 +27,49 @@ const Cart = () => {
     },[])
   return (
     <div>
-      cart
-    <p>{data?.body?.thumbnail}</p>  
-    {console.log("thumbnail",data?.thumbnail)}
-<p> {data?.title}</p>
+        <p>Cart</p>
+     {data && data.map(item=>(
+        <div key={item.id} className="">
+            <div className="thumbmail">
+                <img src={item?.thumbnail} alt={item?.title}/>
+            </div>
+            <div className="h-50 w-50 flex">
+                <div className="image0">
+                <img src={item?.images[0]} alt={item?.title}/>
+
+                </div>
+                <div className="image1">
+                <img src={item?.images[1]} alt={item?.title}/>
+
+                </div>
+                <div className="image1">
+                <img src={item?.images[2]} alt={item?.title}/>
+
+                </div>
+                <div className="image1">
+                <img src={item?.images[3]} alt={item?.title}/>
+
+                </div>
+                
+
+            </div>
+            <div className="title">
+                <div className="innertitel">
+                    {item.title}
+                </div>
+            </div>
+            <div className="innercontain">
+                <p>Price:{item.price}</p>
+                <p>Rating:{item.rating}</p>
+                <p>view:{item.views}</p>
+                <p>Description:`{item.description}</p>
+
+            </div>
+
+        </div>
+        
+     ))}
+   
        
     </div>
   )
